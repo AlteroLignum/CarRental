dd0222a10bfe77515e8c4ceaee14bffeae9667fd
AlteroLignum <hgghfhuff.nj@gmail.com>
2023-12-12 21:10:11 +0400

    Recovery commit

--- src/pages/Models.jsx
+++ src/pages/Models.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import AboutPage from '../components/About/AboutPage'
 
 const Models = () => {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch({type:'CHANGE',payload:'Модели'})
+  },[])
+
   return (
-    <div>Models</div>
+    <div>
+      <AboutPage/>
+      
+    </div>
   )
 }
 
--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import AboutPage from '../components/About/AboutPage'
+import BookAbout from './About/BookAbout'
+import Testimonials from '../components/Testimonials/Testimonials'
 
-const Testimonials = () => {
+const Testimonial = () => {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch({type:'CHANGE',payload:'Отзывы'})
+  },[])
+
   return (
-    <div>Testimonials</div>
+    <div>
+      <AboutPage/>
+      <Testimonials/>
+      <BookAbout/>
+    </div>
   )
 }
 
-export default Testimonials
+export default Testimonial
